145d5a43c2f07330c0cfaedd0da6aa0e4678f4ef
MRadziievska <radzievska.m.a@gmail.com>
2023-11-08 21:53:41 +0200

    add file

---
+++ 2_L.py
@@ -0,0 +1,26 @@
+
+
+n = int(input("Pleas enter the 4-digit number: "))
+
+print("a", n //1000)
+print("b", n // 100 % 10)
+print("c", n // 10 % 10)
+print("d", n % 10)
+
+
+n = int(input("Pleas enter the 5-digit number: "))
+print(n % 10, n // 10 % 10, n // 100 % 10, n // 1000 % 10, n // 10000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
